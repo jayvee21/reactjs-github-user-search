9e591d6cfc108706a127fb93785b692ae5abdb85
Jason Glover <jglover@HerculesGroup.local>
2018-12-05 12:12:41 -0500

    Fetch API from github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+const Card = (props) => {
+  return (
+    <div style={{ margin: '1em', display: 'inline-block'}}>
+      <img width="70" src={props.avatar_url} alt="profile"/>
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>
+          {props.name}
+        </div>
+        <div> {props.location} </div>
+      </div>
+    </div>
+  )
+}
+
+
+const CardList = (props) => {
+  return(
+    <div>
+      { props.cards.map( (card, i) => <Card {...card} key={i}/> )}
+    </div>
+  )
+}
+
+class Form extends Component{
+  state = { username: 'dsadsa'}
+  
+  handleSubmit = (event) => {
+    event.preventDefault()
+    axios.get(`https://api.github.com/users/${this.state.username}`)
+      .then(resp => {
+        this.props.onSubmit(resp.data)
+      })
+  }
+
+  handleOnchange = (event) => {
+    this.setState({ username: event.target.value })
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit} >
+          <input type="text" placeholder="Github username" value={this.state.username} onChange ={this.handleOnchange} required />
+          <button type="submit"> Add card</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
+  state = {
+    cards: []
+  }
+
+  addNewCard = (cardInfo) => {
+    this.setState(prevState => ({ cards: prevState.cards.concat(cardInfo) }))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Form onSubmit = {this.addNewCard} />
+        <CardList cards = {this.state.cards} />
       </div>
     );
   }
